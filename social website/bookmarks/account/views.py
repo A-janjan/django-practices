bb301f46b5b3638eccc13ee6ed50e50df9454ded
Janjan <62621376+A-janjan@users.noreply.github.com>
2022-07-05 13:04:34 +0430

    building a follow system

--- social website/bookmarks/account/views.py
+++ social website/bookmarks/account/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
-from .models import Profile
+from .models import Profile, Contact
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.views.decorators.http import require_POST
+from common.decorators import ajax_required
+
 
 def user_login(request):
     if request.method == "POST":
@@ -43,23 +47,60 @@ def register(request):
             return render(request, "account/register_done.html", {"new_user": new_user})
     else:
         user_form = UserRegistrationForm()
-    return render(request, 'account/register.html',{'user_form':user_form})
+    return render(request, "account/register.html", {"user_form": user_form})
 
 
 @login_required
 def edit(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         user_form = UserEditForm(instance=request.user, data=request.POST)
-        profile_form = ProfileEditForm(instance=request.user.profile,
-                                        data=request.POST,
-                                        files=request.FILES)
+        profile_form = ProfileEditForm(
+            instance=request.user.profile, data=request.POST, files=request.FILES
+        )
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request, 'Profile updated successfully')
+            messages.success(request, "Profile updated successfully")
         else:
-            messages.error(request, 'Error updating your profile')
+            messages.error(request, "Error updating your profile")
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
-    return render(request,'account/edit.html',{'user_form':user_form, 'profile_form':profile_form})
+    return render(
+        request,
+        "account/edit.html",
+        {"user_form": user_form, "profile_form": profile_form},
+    )
+
+
+@login_required
+def user_list(request):
+    profiles = Profile.objects.all()
+    return render(request, 'account/user/list.html', {'section':'people', 'profiles':profiles})
+
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    return render(
+        request, "account/user/detail.html", {"section": "people", "user": user}
+    )
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get("id")
+    action = request.POST.get("action")
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == "follow":
+                Contact.objects.get_or_create(user_from=request.user, user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user, user_to=user).delete()
+            return JsonResponse({"status": "ok"})
+        except User.DoesNotExist:
+            return JsonResponse({"status": "error"})
+    return JsonResponse({"status": "error"})
